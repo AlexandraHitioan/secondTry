ab62d49185e6485625b8e593a5c6587e845e336a
Alexandra <alexandra.hitioan22@gmail.com>
2023-10-07 16:12:07 +0300

    Another small programe

---
+++ src/randomPackage/oneMoreTry.java
@@ -0,0 +1,16 @@
+package randomPackage;
+import java.util.*;
+public class oneMoreTry {
+        public static void main(String[] args)
+        {
+            Scanner sc= new Scanner(System.in);    //System.in is a standard input stream
+            System.out.print("Enter first name- ");
+            String a= sc.next();
+            System.out.print("Enter second name- ");
+            String b= sc.next();
+            System.out.print("Enter third name- ");
+            String c= sc.next();
+            System.out.println(a + " " + b+" " + c);
+        }
+
+}
