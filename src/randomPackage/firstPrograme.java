ebb3259cbe08d7813e5ffcbe2337ce8cdada41f2
Alexandra <alexandra.hitioan22@gmail.com>
2023-10-07 15:35:29 +0300

    Did a print :))

--- src/randomPackage/firstPrograme.java
+++ src/randomPackage/firstPrograme.java
@@ -6,5 +6,7 @@ public class firstPrograme {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
+        System.out.printf("hehehe");
+
     }
 }
